087813075cd68994b6ee055d86a66796509622fd
ishroudd <41129603+ishroudd@users.noreply.github.com>
2020-08-09 01:37:48 -0700

    Added basic support of *.mod files

--- Monad.cpp
+++ Monad.cpp
@@ -2,15 +2,29 @@
 #include <cstdio>
 #include <tlhelp32.h>
 #include <psapi.h>
+#include <cassert>
+#include <map>
+#include <vector>
+#include <sstream>
 #include <iostream>
+#include <fstream>
 
 #pragma comment(lib, "Psapi.lib")
 #define DPSAPI_VERSION (1)
 
 /*************************
-Proof-of-Concept. Currently only proves the ability to successfully write to devil daggers memory space during runtime.
-Excuse any sloppy code.
+Proof-of-Concept. Now pulls a (address, value) pair from a file called modad.mod,
+then writes the value to runtime memory.
+
+Modad.mod is a text file with two DWORD-sized hexidecimal numbers separated by a space. 
+The first DWORD is the RVA from the image base. The second is an AOB (in big-endian) used to patch the given address.
+Very limiting right now without editing the source, still very much a POC.
 ************************/
+
+typedef uintptr_t HEX;
+typedef DWORD ADDR;
+
+HANDLE ddHandle = NULL;
 
 BOOL SetPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege)
 {
@@ -66,7 +80,6 @@ HANDLE GetProcessByName(wchar_t* TargetProcess)
         do {
             if (!wcscmp(process.szExeFile, TargetProcess)){              
                 pid = process.th32ProcessID;
-                //wprintf(L"%ls \n%ls \n%d \n", TargetProcess, process.szExeFile, pid);
                 break;
             }
         } while (Process32Next(snapshot, &process));
@@ -84,7 +97,7 @@ HANDLE GetProcessByName(wchar_t* TargetProcess)
     return NULL;
 }
 
-DWORD GetModule(wchar_t* TargetProcess, HANDLE ProcessHandle)
+ADDR GetModule(wchar_t* TargetProcess, HANDLE ProcessHandle)
 {
     HMODULE hMods[1024];
     DWORD cbNeeded;
@@ -94,9 +107,8 @@ DWORD GetModule(wchar_t* TargetProcess, HANDLE ProcessHandle)
         for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
             if (GetModuleFileNameEx(ProcessHandle, hMods[i], szModPath, sizeof(szModPath) / sizeof(TCHAR))) {
                 if (wcsstr(szModPath, TargetProcess)) {
-                    //wprintf(L"%ls, %ls\n", szModPath, TargetProcess);
 
-                    return (DWORD)hMods[i];
+                    return (ADDR)hMods[i];
                 }
             }
         }
@@ -105,10 +117,18 @@ DWORD GetModule(wchar_t* TargetProcess, HANDLE ProcessHandle)
     return NULL;
 }
 
+// functor that defines a virtual address based on the image base and a given offset
+struct virtual_addr {
+    virtual_addr(ADDR ImageBase) : base(ImageBase) {}
+    ADDR operator()(uintptr_t offset) const { return base + offset; }
+
+private:
+    ADDR base;
+};
+
 int main(int argc, char* argv[])
 {
     HANDLE TokenHandle = NULL;
-    HANDLE ddHandle = NULL;
     wchar_t ProcessName[7] = L"dd.exe";
     
     if (!(ddHandle = GetProcessByName(ProcessName))) {
@@ -125,20 +145,55 @@ int main(int argc, char* argv[])
         return FALSE;
     }
 
-    DWORD ddBase = GetModule(ProcessName, ddHandle);
-    printf("%d\n", ddBase);
+    // ImageBase for dd
+    ADDR ddBase = GetModule(ProcessName, ddHandle);
 
-    // Offset from base address to address used for proof-of-concept.
-    DWORD POCAddress = ddBase + 618755;
-    printf("%d\n", POCAddress);
-    
-    // little-endian '83 E4 FF' in decimal form.
-    int POCOffset = 16770179;
+    // Taking values from mod file
+    std::ifstream ModFile;
+    ModFile.open("modad.mod", std::ios_base::in);
 
-    WriteProcessMemory(ddHandle, (LPVOID) POCAddress, &POCOffset, 3, NULL);
-    ReadProcessMemory(ddHandle, (LPCVOID)POCAddress, &POCOffset, 3, NULL);
-    printf("%d", POCOffset);
+    std::vector<std::string> data;
+    std::string temp;
+
+    while(ModFile >> temp) {
+        data.push_back(temp);
+    }
+    ModFile.close();
+
+    ADDR ModTarget;
+    std::stringstream Addrss;
+    Addrss << data[0];
+    Addrss >> std::hex >> ModTarget;
+    printf("num: %d\n", ModTarget);
+
+    virtual_addr rva(ddBase);
+
+    ADDR TargetAddress = rva(ModTarget);
+    printf("TargetAddr: %d\n", TargetAddress);
+
+    // Convert AOB to little-endian
+    assert(data[1].length() % 2 == 0);
+    std::reverse(data[1].begin(), data[1].end());
+    for (auto it = data[1].begin(); it != data[1].end(); it += 2) {
+        std::swap(it[0], it[1]);
+    }
+
+    // Convert hex string to int typedef
+    std::cout << std::hex << data[1] << std::endl;
+    HEX HexBytes;
+    std::stringstream Hexss;
+    Hexss << std::hex << data[1];
+    Hexss >> HexBytes;
+    HEX HexBytes2 = HexBytes;
+
+    // Writes to runtime memory, then reads the new value for confirmation
+    int n;
+    WriteProcessMemory(ddHandle, (LPVOID) TargetAddress, (LPCVOID) &HexBytes, 4, NULL);
+    ReadProcessMemory(ddHandle, (LPCVOID) TargetAddress, (LPVOID) &HexBytes, 4, NULL);
+    std::cout << "Final byte values: " << static_cast<int>(HexBytes) << std::endl;
 
     CloseHandle(ddHandle);
+
+    getchar();
     return 0;
 }
